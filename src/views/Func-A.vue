56f85945352b087cf32de8724197b1df64871508
yirube <yusuke.irube@dxc.com>
2020-01-27 10:59:24 +0900

    2020.01.27 commit second

--- src/views/Func-A.vue
+++ src/views/Func-A.vue
@@ -8,7 +8,43 @@
         </v-card>
       </v-col>
       <v-col cols="9">
-        <v-card>other contents</v-card>
+        <v-card>
+          <v-card-title>
+            Item Description
+            <v-spacer></v-spacer>
+          </v-card-title>
+          <v-form class="pl-5 pr-5 pb-5" ref="form" v-model="valid">
+            <v-text-field label="Field-01" required></v-text-field>
+            <v-text-field label="Field-02"></v-text-field>
+
+            <div style="display:inline-flex">
+              <v-text-field class="mr-2" label="Field-03" required></v-text-field>
+              <v-text-field class="mr-2" label="Field-04"></v-text-field>
+              <v-text-field class="mr-2" label="Field-05"></v-text-field>
+            </div>
+
+            <v-select
+              v-model="select"
+              :items="items"
+              :rules="[v => !!v || 'Item is required']"
+              label="Item"
+              required
+            ></v-select>
+
+            <v-checkbox
+              v-model="checkbox"
+              :rules="[v => !!v || 'You must agree to continue!']"
+              label="Term of Use"
+              required
+            ></v-checkbox>
+
+            <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
+
+            <v-btn color="error" class="mr-4" @click="reset">Reset Form</v-btn>
+
+            <v-btn color="warning" @click="resetValidation">Reset Validation</v-btn>
+          </v-form>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -25,6 +61,9 @@ export default Vue.extend({
   components: {
     TreeView,
     MenuFuncA
-  }
+  },
+  data: () => ({}),
+
+  methods: {}
 });
 </script>
